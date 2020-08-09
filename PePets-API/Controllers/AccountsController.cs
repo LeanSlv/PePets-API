e52343f3b2641911bf19642fb2ec62a771209f27
Mihail Solovev <youoliver@mail.ru>
2020-08-09 13:17:07 +0400

    Added a method and models for getting an authorization token

---
+++ PePets-API/Controllers/AccountsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using PePets_API.Models;
+using PePets_API.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PePets_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountsController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Token([FromBody]LoginViewModel model)
+        {
+            var identity = await GetIdentity(model.UserName, model.Password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return Ok(encodedJwt);
+        }
+        private async Task<IReadOnlyCollection<Claim>> GetIdentity(string userName, string password)
+        {
+            List<Claim> claims = null;
+            
+            return claims;
+        }
+
+
+
+
+
+
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PePets-API/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PePets_API.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
